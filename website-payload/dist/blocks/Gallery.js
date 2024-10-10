444ba24b598f8fcfbe3200451545df5d4c37a3a1
arekasek <arekas2914@wp.pl>
2024-10-10 20:13:45 +0200

    new design

---
+++ website-payload/dist/blocks/Gallery.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Gallery = void 0;
+exports.Gallery = {
+  slug: "gallery",
+  labels: {
+    singular: "Gallery",
+    plural: "Galleries",
+  },
+  fields: [
+    {
+      name: "heading",
+      label: "Heading",
+      type: "text",
+      maxLength: 60,
+    },
+    {
+      name: "color",
+      label: "Color (HEX)",
+      type: "text",
+      required: false,
+    },
+    {
+      name: "text",
+      label: "Text",
+      type: "textarea",
+    },
+    {
+      name: "image",
+      label: "Image",
+      type: "upload",
+      relationTo: "media",
+      required: true,
+    },
+    {
+      name: "images",
+      label: "Images",
+      type: "array",
+      minRows: 1,
+      fields: [
+        {
+          name: "image",
+          label: "Image",
+          type: "upload",
+          relationTo: "media",
+          required: true,
+        },
+      ],
+    },
+    {
+      name: "direction",
+      label: "Direction",
+      type: "select",
+      options: [
+        {
+          label: "Left",
+          value: "left",
+        },
+        {
+          label: "Right",
+          value: "right",
+        },
+      ],
+    },
+  ],
+};
